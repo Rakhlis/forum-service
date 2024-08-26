cb57cdee8260f167c0dfac89a1bdc0d7a695dc36
dmitrijrahlis <dmitrijrahlis@localhost>
2024-08-26 15:22:12 +0500

    accounting was started

---
+++ src/main/java/telran/java53/accounting/service/UserAccountService.java
@@ -0,0 +1,25 @@
+package telran.java53.accounting.service;
+
+import java.util.Set;
+
+import telran.java53.accounting.dto.RolesDto;
+import telran.java53.accounting.dto.UserDto;
+import telran.java53.accounting.dto.UserEditDto;
+import telran.java53.accounting.dto.UserRegisterDto;
+
+public interface UserAccountService {
+
+	UserDto register(UserRegisterDto userRegisterDto);
+	
+	UserDto getUser(String login);
+	
+	UserDto removeUser(String login);
+	
+	UserDto updateUser(String login, UserEditDto userEditDto);
+	
+	RolesDto changeRolesList(String login, String role, boolean isAddRoles);
+	
+	void changePassword(String login, String password);
+	
+	
+}
